dc57ca02da94e65a48c66e8777dfbfb6d770fcdb
Joao Martins <milanezijoao51@gmail.com>
2024-01-16 10:13:53 -0300

    Criando CategoryController

---
+++ ComerceAPI/Controllers/CategoryController.cs
@@ -0,0 +1,70 @@
+﻿using ComerceAPI.Context;
+using ComerceAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComerceAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController:ControllerBase
+    {
+        private readonly AppDBContext _dbContext;
+
+        public CategoryController(AppDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Category>> Get()
+        {
+            return await _dbContext.Categories.ToListAsync();
+        }
+        [HttpGet ("id:int")]
+        public async Task<ActionResult<Category>> Get(int id)
+        {
+            var categoryId= await _dbContext.Categories.FindAsync(id);
+            if(categoryId is null) {
+                return  NotFound();
+            }
+            return Ok(categoryId);
+        }
+        [HttpPost]
+        public async Task<ActionResult<Category>> Post(Category category) 
+        {
+            if(category == null)
+            {
+                return BadRequest();
+            }
+            await _dbContext.Categories.AddAsync(category);
+            await _dbContext.SaveChangesAsync();
+            return Ok(category);
+        }
+        [HttpPut ("id:int")]
+        public async Task<ActionResult<Category>> Put(int id,Category category)
+        {
+            
+            if (category.Id != id)
+            {
+                return BadRequest();
+            }
+            var categoryId = await _dbContext.Categories.FindAsync(id);
+            categoryId.Name =category.Name;
+            await _dbContext.SaveChangesAsync();
+            return Ok(categoryId);
+        }
+        [HttpDelete]
+        public async Task<ActionResult<Category>> Delete(int id)
+        {
+            var categoryId = await _dbContext.Categories.FindAsync(id);
+            if(categoryId is null)
+            {
+                return BadRequest();
+            }
+             _dbContext.Categories.Remove(categoryId);
+            await _dbContext.SaveChangesAsync();
+            return NotFound();
+        }
+    }
+}
--- ComerceAPI/Model/Category.cs
+++ ComerceAPI/Model/Category.cs
@@ -1,9 +1,12 @@
-﻿namespace ComerceAPI.Model
+﻿using System.Text.Json.Serialization;
+
+namespace ComerceAPI.Model
 {
     public class Category
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+        [JsonIgnore]
         public List<Product> Products { get; set; } = new List<Product>();
     }
 }
